8e461d43ae6a21df9ca109869902c12dc89facde
Michael Chen <mich0919@PC-Chen19.local>
2020-03-11 11:39:53 -0500

    Updates

---
+++ precache-manifest.4a75172dfaee00c085c1f8fb9e49c5b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b1c9f9e5e3756ea3c19592b00d6f323",
+    "url": "/track-the-pack/index.html"
+  },
+  {
+    "revision": "6208855abedf085e4d90",
+    "url": "/track-the-pack/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "917557ebff045734aecc",
+    "url": "/track-the-pack/static/css/main.2153ad7a.chunk.css"
+  },
+  {
+    "revision": "6208855abedf085e4d90",
+    "url": "/track-the-pack/static/js/2.f1aedc75.chunk.js"
+  },
+  {
+    "revision": "917557ebff045734aecc",
+    "url": "/track-the-pack/static/js/main.10f78b45.chunk.js"
+  },
+  {
+    "revision": "0fb1a7c29c7751feaa07",
+    "url": "/track-the-pack/static/js/runtime-main.10436a33.js"
+  }
+]);
