d84a1e18558fcb5f6840e718afd39d34f52b6c5f
Michael Chen <mich0919@pc-chen19.lan>
2020-03-18 15:51:28 -0500

    Updates

--- precache-manifest.b40ed1f9f926994bd065be89ad7c394d.js
+++ precache-manifest.b40ed1f9f926994bd065be89ad7c394d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9873006e068b62fce4b568f8d69a1884",
+    "revision": "5dd5aec9139eefb2665ff48fa9e6319a",
     "url": "/track-the-pack/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/track-the-pack/static/css/2.b0bb7f2c.chunk.css"
   },
   {
-    "revision": "939dd97ab2672bf19223",
-    "url": "/track-the-pack/static/css/main.2153ad7a.chunk.css"
+    "revision": "65500fdc27bd0fcafd1f",
+    "url": "/track-the-pack/static/css/main.268e18e0.chunk.css"
   },
   {
     "revision": "6208855abedf085e4d90",
     "url": "/track-the-pack/static/js/2.f1aedc75.chunk.js"
   },
   {
-    "revision": "939dd97ab2672bf19223",
-    "url": "/track-the-pack/static/js/main.6a6351c0.chunk.js"
+    "revision": "65500fdc27bd0fcafd1f",
+    "url": "/track-the-pack/static/js/main.81ff525b.chunk.js"
   },
   {
     "revision": "0fb1a7c29c7751feaa07",
